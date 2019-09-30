1b5c4763a3cabe58e1cb46a54126a2619ea7bf1f
bwallace <17948072+brandon-wallace@users.noreply.github.com>
2019-09-29 21:25:20 -0400

    Check for errors
    Check HTTP response for success code of 200 or 40? not found
    error.

--- right_status.sh
+++ right_status.sh
@@ -1,15 +1,26 @@
 #!/bin/bash 
 
 
-function forcast() {
-    WEATHER=$(curl --silent
-    "http://api.openweathermap.org/data/2.5/forecast?id=5110253&appid=<your_api_key>&units=imperial")
-    FAHRENHEIT=$(printf "%s" ${WEATHER} | jq '.list[0].main.temp')
-    COUNTRY=$(printf "%s" ${WEATHER} | jq -r '.city.country')
-    CITY=$(printf "%s" ${WEATHER} | jq -r '.city.name')
-    FORECAST=$(printf "%s %s" ${WEATHER} | jq -r '.list[0].weather[0].description')
-    printf "%s %s %s %s %s %s" "${FAHRENHEIT:=""}°F" '/' "${CITY:=""}" "${COUNTRY:=""}" '/' "${FORECAST:=""}"
+function forecast() {
+    WEATHER=$(curl --silent "http://api.openweathermap.org/data/2.5/forecast?id=5110253&appid=&units=imperial")
+
+    RESPONSE=$(printf %s ${WEATHER} | jq -r '.cod')
+
+    case $RESPONSE in
+        200)
+            FAHRENHEIT=$(printf "%s" ${WEATHER} | jq '.list[0].main.temp')
+            COUNTRY=$(printf "%s" ${WEATHER} | jq -r '.city.country')
+            CITY=$(printf "%s" ${WEATHER} | jq -r '.city.name')
+            FORECAST=$(printf "%s %s" ${WEATHER} | jq -r '.list[0].weather[0].description')
+            printf "%s %s %s %s %s %s" "${FAHRENHEIT:=""}°F" '/' "${CITY:=""}" "${COUNTRY:=""}" '/' "${FORECAST:=""}"
+            ;;
+        401)
+            printf "%s %s %s %s %s %s" "${FAHRENHEIT:="??"}°F" '/' "${CITY:="Bronx"}" "${COUNTRY:="US"}" '/' "${FORECAST:="unknown"}"
+            ;;
+        *)
+            printf "%s" "Error"
+    esac
 }
 
 
-forcast
+forecast
