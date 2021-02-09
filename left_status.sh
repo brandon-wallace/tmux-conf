d182d24dce15311c0340ec191d15867164bde91a
Brandon Wallace <17948072+brandon-wallace@users.noreply.github.com>
2021-02-09 16:46:11 -0500

    Update left_status.sh
    Removed hard coded network interface.
    Added comments for others to understand the code better.
    Removed an unnecessary function.
    Made code more modular so that certain functions could get called by a main function.

--- left_status.sh
+++ left_status.sh
@@ -1,45 +1,94 @@
 #!/bin/bash -
 
 
-function install_dependencies() {
+function ip_address() {
 
-    # Check to make sure dependencies are installed.
-    for a in 'bc' 'sensors' 'acpi'; do
+    interface_array=()
 
-        [ "(which $a)" ] || printf "%sPlease install ${a}."
+    # Loop through the interfaces available.
+    for i in $(ls -1 /sys/class/net/); do
+
+        # Add every interface to the array except for the loopback device.
+        if [ "$i" != 'lo' ]; then
+
+            interface_array+=($i)
+
+        fi
 
     done
+
+    # Check to see which interface is up.
+    for j in ${interface_array[@]}; do
+
+        if [ "$(cat /sys/class/net/${j}/operstate)" == "up" ]; then
+
+            iface="${j}"
+
+        else
+
+            iface=""
+
+        fi
+
+    done
+
+    if [ ! -z "$iface" ]; then
+
+        # Display the IP address.
+        printf "%s " "$(ip addr show $iface | awk '/inet /{print $2}')" 
+
+    fi
 
 }
 
 
-function temperature_levels() {
-
-    # Display temperature of the CPU cores.
-    sensors -f | awk '/Core 0/{printf $3" "}/Core 1/{printf $3" "}'
+function memory_levels() {
 
     # Display memory amount used / total memory and percentage.
     read used total <<< $(free -m | awk '/Mem/{printf $2" "$3}')
 
-    fgcolor='#[fg=brightblue]'
+    fgcolor=''
+    percent=''
 
-    case $percent in
+    # If bc is installed calculate the percentage of memory used.
+    if [ "$(which bc)" ]; then
+    
+        percent=$(bc -l <<< "100 * $total / $used")
 
-        # Warn user if memory is consuming more than 75%!
-        7[5-9].*|8[0-9].*|9[0-9].*) fgcolor='#[fg=brightred]'
-            ;;
+        case $percent in
 
-    esac
+            # Warn user if memory is consuming more than 75%!
+            7[5-9].*|8[0-9].*|9[0-9].*) fgcolor='#[fg=brightred]'
+                ;;
 
-    awk -v u=$used -v t=$total -v fg=$fgcolor -v p=$percent 'BEGIN {printf "%s/%sMB %s %.1f%", t, u, fg, p}'
+        esac
+
+    fi
+
+    awk -v u=$used -v t=$total -v fg=$fgcolor -v p=$percent 'BEGIN {printf "%s/%sMB%s %.1f%", t, u, fg, p}'
+
+}
+
+
+function cpu_temperature() {
+
+    # If sensors program is installed check the CPU core temperature.
+    if [ "$(which sensors)" ]; then
+
+        # Display temperature of the CPU cores.
+        sensors -f | awk '/Core 0/{printf $3" "}/Core 1/{printf $3" "}'
+
+    fi
 
 }
 
 
 function battery_levels() {
 
-        fgdefault='#[fg=brightblue]'
+    # If acpi program is installed check the battery levels.
+    if [ "$(which acpi)" ]; then
 
+        # Show a plus sign if the battery is charging otherwise show a minus sign.
         if [ "$(cat /sys/class/power_supply/AC/online)" == 1 ] ; then
 
             charging='+' 
@@ -57,100 +106,77 @@ function battery_levels() {
 
             case $batt0 in
 
-                100%|9[0-9]%|8[0-9]%|7[5-9]%) fgcolor='#[fg=brightblue]'
+                # 100% - 75% => blue
+                100%|9[0-9]%|8[0-9]%|7[5-9]%) fgcolor='#[fg=brightblue]' 
                     ;;
-                7[0-4]%|6[0-9]%|5[0-9]%) fgcolor='#[fg=brightgreen]'
+                # 74% - 50% => green
+                7[0-4]%|6[0-9]%|5[0-9]%) fgcolor='#[fg=brightgreen]' 
                     ;;
-                4[0-9]%|3[0-9]%|2[5-9]%) fgcolor='#[fg=brightyellow]'
+                # 49% - 25% => yellow
+                4[0-9]%|3[0-9]%|2[5-9]%) fgcolor='#[fg=brightyellow]' 
                     ;;
-                2[0-4]%|1[0-9]%|[0-9]%) fgcolor='#[fg=brightred]'
+                # 24% - 0% => red
+                2[0-4]%|1[0-9]%|[0-9]%) fgcolor='#[fg=brightred]' 
                     ;;
 
             esac
 
             # Display the percentage of charge the battery has.
-            printf "/ ${fgcolor}${charging}%s $fgdefault" "$batt0"
+            printf " ${fgcolor}${charging}%s $fgdefault" "$batt0"
 
         fi
 
-        # Check for existence of a second battery.
+        # Check for existence of a second battery. Some laptops have two batteries.
         if [ -x /sys/class/power_supply/BAT1 ] ; then
 
             batt1=$(acpi -b 2> /dev/null | awk '/Battery 1/{print $4}' | cut -d, -f1)
 
             case $batt1 in
-
-                100%|9[0-9]%|8[0-9]%|7[5-9]%) fgcolor='#[fg=brightblue]'
+               
+                # 100% - 75% => blue
+                100%|9[0-9]%|8[0-9]%|7[5-9]%) fgcolor=>'#[fg=>brightblue]' 
                     ;;
-                7[0-4]%|6[0-9]%|5[0-9]%) fgcolor='#[fg=brightgreen]'
+                # 74% - 50% => green
+                7[0-4]%|6[0-9]%|5[0-9]%) fgcolor=>'#[fg=>brightgreen]' 
                     ;;
-                4[0-9]%|3[0-9]%|2[5-9]%) fgcolor='#[fg=brightyellow]'
+                # 49% - 25% => yellow
+                4[0-9]%|3[0-9]%|2[5-9]%) fgcolor=>'#[fg=>brightyellow]' 
                     ;;
-                2[0-4]%|1[0-9]%|[0-9]%) fgcolor='#[fg=brightred]'
+                # 24% - 0% => red
+                2[0-4]%|1[0-9]%|[0-9]%) fgcolor=>'#[fg=>brightred]' 
                     ;;
 
             esac
 
             # Display the percentage of charge the battery has.
-            printf "/ ${fgcolor}${charging}%s $fgdefault" "$batt1"
+            printf " ${fgcolor}${charging}%s $fgdefault" "$batt1"
 
         fi
-
-}
-
-
-function vpn_connection() {
-
-    # Check for tun0 interface.
-    [ -d /sys/class/net/tun0 ] && printf "/ %s " 'VPN*'
-
-}
-
-
-function tmux_left_status() {
-
-    # Create an empty array.
-    interface_arr=()
-
-    # Check available interfaces.
-    for i in $(ls -1 /sys/class/net/); do
-
-        # Add every interface to the array except for the loopback device.
-        if [ "$i" != 'lo' ]; then
-
-            interface_arr+=($i)
-
-        fi
-
-    done
-
-    # Check for interfaces that are up.
-    for j in ${interface_arr[@]}; do
-
-        if [ "$(cat /sys/class/net/${j}/operstate)" == "up" ]; then
-
-            iface="${j}"
-
-        else
-
-            iface=""
-
-        fi
-
-    done
-
-    # Print the ip address of the interface.
-    if [ ! -z "$iface" ]; then
-
-        printf "%s " "$(ip addr show $iface | awk '/inet /{print $2}')" 
 
     fi
 
-    cpu_memory_levels
-    vpn_connection
-    battery_levels
+}
+
+
+function is_vpn_enabled() {
+
+    # Check for tun0 interface.
+    [ -d /sys/class/net/tun0 ] && printf " %s" 'VPN*'
 
 }
 
-install_dependencies
-tmux_left_status
+
+function main() {
+
+    # Comment out any of the functions not needed.
+    # If running this on a desktop computer battery level is not needed.
+    ip_address
+    battery_levels
+    cpu_temperature
+    memory_levels
+    is_vpn_enabled
+
+}
+
+
+main
