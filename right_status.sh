8138dc43fa7adb161fec756df18869c2730c08f9
Brandon Wallace <17948072+brandon-wallace@users.noreply.github.com>
2021-03-28 15:49:46 -0400

    config: Add colors to status bar
    Colors were added to some of the functions.

--- right_status.sh
+++ right_status.sh
@@ -1,27 +1,31 @@
 #!/bin/bash 
 
 
-function forecast() {
-    WEATHER=$(curl --silent
-    "http://api.openweathermap.org/data/2.5/forecast?id=5110253&appid=<your-api-key>&units=imperial")
+function load_average() {
 
-    RESPONSE=$(printf %s ${WEATHER} | jq -r '.cod')
+    local fgcolor='#[fg=colour40]'
 
-    case $RESPONSE in
-        200)
-            FAHRENHEIT=$(printf "%s" ${WEATHER} | jq '.list[0].main.temp')
-            COUNTRY=$(printf "%s" ${WEATHER} | jq -r '.city.country')
-            CITY=$(printf "%s" ${WEATHER} | jq -r '.city.name')
-            FORECAST=$(printf "%s %s" ${WEATHER} | jq -r '.list[0].weather[0].description')
-            printf "%s %s %s %s %s %s" "${FAHRENHEIT:=""}°F" '/' "${CITY:=""}" "${COUNTRY:=""}" '/' "${FORECAST:=""}"
-            ;;
-        401)
-            printf "%s %s %s %s %s %s" "${FAHRENHEIT:="??"}°F" '/' "${CITY:="Bronx"}" "${COUNTRY:="US"}" '/' "${FORECAST:="unknown"}"
-            ;;
-        *)
-            printf "%s" "Error"
-    esac
+    printf "%s " "${fgcolor}$(uptime | awk -F: '{printf $NF}' | tr -d ',')"
+
 }
 
+function date_time() {
 
-forecast
+    local fgcolor='#[fg=colour202]'
+
+    printf "%s" "${fgcolor}$(date +'%Y-%m-%d %H:%M:%S %Z')"
+
+}
+
+function main() {
+
+    display_temperature
+    #battery_meter
+    load_average
+    date_time
+
+}
+
+# Calling the main function which will call the other functions.
+main
+
